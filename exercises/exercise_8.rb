778f7b8c8d138b0473d560b283d6d5e0a97f57eb
Nick Yu <yynickel@gmail.com>
2019-07-10 02:43:17 +0000

    exercise 8 done

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,6 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_4'
+require_relative './exercise_6'
+
+puts "Here is the password of first employee: #{Employee.first.password}"
--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,23 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store, presence: true
   validates :hourly_rate, numericality: {greater_than: 40, less_than: 200, only_integer:true}
 
+  # before_create :generate_password
+  # private
+  #   def generate_password
+  #     self.password=[*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+  #   end
+
+  # after_create :generate_password
+  # private
+  #   def generate_password
+  #     self.password=[*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+  #     self.save # need to add this line to make after_create work
+  #   end
+
+  before_save :generate_password
+  private
+    def generate_password
+      self.password=[*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+    end
+
 end
